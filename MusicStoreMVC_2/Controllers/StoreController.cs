e060068f3711e86aa673399e5e3c7610a5e7ac73
hdduong <hdduong.at.wpi.edu@gmail.com>
2014-03-08 23:27:43 -0500

    View and Model

--- MusicStoreMVC_2/Controllers/StoreController.cs
+++ MusicStoreMVC_2/Controllers/StoreController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MusicStoreMVC_2.Models;
 
 namespace MusicStoreMVC_2.Controllers
 {
@@ -13,8 +14,27 @@ namespace MusicStoreMVC_2.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var genres = new List<Genre> {
+                new Genre { Name = "Disco"}, 
+                new Genre { Name = "Jazz"},
+                new Genre { Name = "Rock"}
+            };
+
+            return View(genres);
         }
 
+        public ActionResult Details(int id) // GET /Browse/Details/5
+        {
+            var album = new Album { Title = "Album " + id };
+
+            return View(album);
+
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre{Name = genre};
+            return View(genreModel);
+        }
     }
 }
